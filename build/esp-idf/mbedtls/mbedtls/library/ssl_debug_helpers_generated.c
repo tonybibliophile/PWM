6508e1bb278fe9dcc550cbc7c06b7bdd015d6e82
Tony <tonytonypig808@gmail.com>
2025-07-31 22:27:37 +0800

    refresh

--- build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/Users/laizhiquan/esp/v5.4.2/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
--- build/esp-idf/mbedtls/mbedtls/library/version_features.c
+++
@@ -1,1 +0,0 @@
-/Users/laizhiquan/esp/v5.4.2/esp-idf/components/mbedtls/mbedtls/library/version_features.c
